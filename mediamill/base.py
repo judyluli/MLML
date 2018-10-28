00f18033edfe46db3b3777df1705841aa79d5c57
judyluli <1072465199@qq.com>
2018-10-28 19:57:00 +0800

    3rd

--- mediamill/base.py
+++ mediamill/base.py
@@ -4,7 +4,7 @@ sys.path.append("..")
 from acehelp import acev1
 import input
 model = acev1.ACE(rate=0.2)
-model.set_trainParameters(logdir='/home/lulusee/MLML/mediamill/log.txt')
+model.set_trainParameters(epoches=10,logdir='/home/lulusee/MLML/mediamill/log.txt')
 data_x,data_y = input.readfile('/data/users/lulusee/mediamill/mediamill-train.arff')
 test_x,test_y = input.readfile('/data/users/lulusee/mediamill/mediamill-test.arff')
 model.start_graph(data_x,data_y,test_x,test_y)
